a9cb4368fd737822f6703544d83a8061733b72ec
aevesque <141275616+aevesque@users.noreply.github.com>
2026-06-08 19:48:19 +0200

    minor fixes

--- srcs/main.c
+++ srcs/main.c
@@ -7,7 +7,7 @@ void	renderFrame(Context *context, TermGL termGL)
 	for (int i = 0; i < context->control_point_count; ++i)
 	{
 		control_points[i] = toAbsoluteUnbound(context->control_points[i], DISPLAY(termGL));
-		if (control_points[i].x >= 0 && control_points[i].x < getDisplayWidth(termGL) && control_points[i].y >= 0 && control_points[i].y < getDisplayHeight(termGL))
+		if (control_points[i].x >= 0 && control_points[i].x < (int)getDisplayWidth(termGL) && control_points[i].y >= 0 && control_points[i].y < (int)getDisplayHeight(termGL))
 			setPixelZBuffered(control_points[i].x, control_points[i].y, ALWAYS_ON_TOP, (i == context->current_point ? SEL_POINT_COLOR : POINT_COLOR), DISPLAY(termGL));
 	}
 
@@ -15,7 +15,7 @@ void	renderFrame(Context *context, TermGL termGL)
 	{
 		const fVec3	p = nBezier(control_points, context->control_point_count, t);
 		if ((int)p.x >= 0 && p.x < getDisplayWidth(termGL) && (int)p.y >= 0 && p.y < getDisplayHeight(termGL))
-			setPixel(p.x, p.y, LINE_COLOR, DISPLAY(termGL));
+			setPixelZBuffered(p.x, p.y, ZBUF_MAX, LINE_COLOR, DISPLAY(termGL));
 	}
 
 	displayGeneralInfo(context, termGL);
@@ -34,7 +34,7 @@ int main(void)
 {
 	TermGL	termGL = termGLInit(100, 100);
 	Context context = {
-		.control_points = calloc(sizeof(fVec3), 2),
+		.control_points = calloc(2, sizeof(fVec3)),
 		.control_point_count = 2,
 		.curve_resolution = STARTING_CURVE_RESOLUTION,
 		.curve_point_count = 1 / STARTING_CURVE_RESOLUTION,
@@ -54,4 +54,5 @@ int main(void)
 
 	free(context.control_points);
 	termGLDestroy(termGL);
+	return (0);
 }
